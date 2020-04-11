60aff78af5f63d40b327f323cb15980b892527d1
Taulant Ibraj <tibraj94@gmail.com>
2020-04-11 15:55:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
 # Add your code here
+class Dog
+attr_accessor :name
+@@all = []
+def initialize(name)
+  @name = name
+  @@all << self
+end
+def self.all #From inside the Dog class, we can access the @@all class variable, but whenever we might be interacting with our Dog class from the outside, this .all class method acts as our direct interface to the @@all variable.
+  @@all
+end
+def self.print_all(all)
+  all.each do |dog|
+    puts "#{dog}"
+  end
+end
+end
