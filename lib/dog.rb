8c7bf71fb456ce14d25cebf27e5342b4a4533da3
Taulant Ibraj <tibraj94@gmail.com>
2020-04-11 15:58:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,9 +12,12 @@ end
 def self.all #From inside the Dog class, we can access the @@all class variable, but whenever we might be interacting with our Dog class from the outside, this .all class method acts as our direct interface to the @@all variable.
   @@all
 end
-def self.print_all(all)
-  all.each do |dog|
+def self.print_all
+  @@all.each do |dog|
     puts "#{dog}"
   end
+end
+def self.clear_all
+  @@all.clear
 end
 end
